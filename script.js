551a571e8474d92570187e8cdfffdded4d11f6df
Keitinn <keitinn.reader@gmail.com>
2022-03-20 22:00:19 +0900

    フォーマッターで整形

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let taskListElem; // どこからでも読み書きできるように外側に変数を移動
 
 let tasks = [];
-window.addEventListener("load", function () {
+window.addEventListener('load', function () {
   // リストを取得
-  taskListElem = document.querySelector("ul");
+  taskListElem = document.querySelector('ul');
   // タスクを追加
 
   // LocalStorageから配列を読み込む
@@ -15,51 +15,51 @@ window.addEventListener("load", function () {
 
 function renderTasks() {
   // ul要素の中身をクリア
-  taskListElem.innerHTML = "";
+  taskListElem.innerHTML = '';
 
   let numOfCompletedTasks = 0;
   // 操作された内容で再度li要素を追加
   for (let task of tasks) {
     // リストの項目を作成
-    let taskElem = document.createElement("li");
+    let taskElem = document.createElement('li');
     taskElem.innerText = task.name;
 
     // 項目をダブルクリックされたときの動作を設定
-    taskElem.addEventListener("dblclick", function () {
+    taskElem.addEventListener('dblclick', function () {
       // リストの項目をダブルクリックされたときは、タスクを削除
       deleteTask(task.name); // 【変更点】
     });
 
     // 項目をクリックされたときの動作を設定
-    taskElem.addEventListener("click", function () {
+    taskElem.addEventListener('click', function () {
       // リストの項目をクリックされたときは、タスクの完了状態をトグル
       toggleTaskComplete(task.name);
     });
 
     // タスクの完了状態に応じて項目に取り消し線を引く
     if (task.isCompleted) {
-      taskElem.style.textDecorationLine = "line-through";
+      taskElem.style.textDecorationLine = 'line-through';
       numOfCompletedTasks++;
     } else {
-      taskElem.style.textDecorationLine = "none";
+      taskElem.style.textDecorationLine = 'none';
     }
 
     // 期限表示を作成
-    let taskDueDateElem = document.createElement("span");
-    taskDueDateElem.style.fontSize = "0.8rem";
-    taskDueDateElem.style.fontStyle = "italic";
-    taskDueDateElem.style.marginLeft = "1rem";
+    let taskDueDateElem = document.createElement('span');
+    taskDueDateElem.style.fontSize = '0.8rem';
+    taskDueDateElem.style.fontStyle = 'italic';
+    taskDueDateElem.style.marginLeft = '1rem';
     if (task.dueDate) {
       taskDueDateElem.innerText = task.dueDate;
     } else {
-      taskDueDateElem.innerText = "";
+      taskDueDateElem.innerText = '';
     }
 
     // 期限に応じて文字色を変える
     compareDueDate(task, taskDueDateElem);
 
     // 期限の残日数を表示する
-    let taskRemainingDaysElm = document.createElement("span");
+    let taskRemainingDaysElm = document.createElement('span');
     let nowDateTime = new Date().getTime();
     let taskDateTime = new Date(task.dueDate).getTime();
 
@@ -75,12 +75,12 @@ function renderTasks() {
       nowDate.toLocaleDateString() == taskDate.toLocaleDateString();
 
     if (task.dueDate && isOverdue) {
-      taskRemainingDaysElm.innerText = "残り日数:" + taskRemainingDays + "日";
+      taskRemainingDaysElm.innerText = '残り日数:' + taskRemainingDays + '日';
     } else {
-      taskRemainingDaysElm.innerText = "";
+      taskRemainingDaysElm.innerText = '';
     }
 
-    taskRemainingDaysElm.style.marginLeft = "1rem";
+    taskRemainingDaysElm.style.marginLeft = '1rem';
 
     // 項目に対し、期限表示を追加
     taskElem.appendChild(taskDueDateElem);
@@ -93,10 +93,10 @@ function renderTasks() {
   }
 
   // 全タスクの件数を更新
-  let numOfTasksElm = document.querySelector("#numOfTasks");
+  let numOfTasksElm = document.querySelector('#numOfTasks');
   numOfTasksElm.innerText = tasks.length;
   // 完了済みの件数を更新
-  let numOfCompletedTasksElm = document.querySelector("#numOfCompletedTasks");
+  let numOfCompletedTasksElm = document.querySelector('#numOfCompletedTasks');
   numOfCompletedTasksElm.innerText = numOfCompletedTasks;
 
   // 期日に応じて文字色を変える
@@ -104,11 +104,11 @@ function renderTasks() {
     let nowDate = new Date();
     let taskDate = new Date(task.dueDate);
     if (nowDate.toLocaleDateString() == taskDate.toLocaleDateString()) {
-      taskDueDateElem.style.color = "#f9ca24";
+      taskDueDateElem.style.color = '#f9ca24';
     } else if (nowDate > taskDate) {
-      taskDueDateElem.style.color = "#EA2027";
+      taskDueDateElem.style.color = '#EA2027';
     } else {
-      taskDueDateElem.style.color = "black";
+      taskDueDateElem.style.color = 'black';
     }
   }
 }
@@ -124,7 +124,7 @@ function addTask(taskName, taskDueDate) {
 
   if (isDuplicate) {
     // 配列にすでに同じ名前の項目がある
-    alert("すでに登録済みです");
+    alert('すでに登録済みです');
     return;
   }
   // 配列に対し、項目を追加　【変更点】
@@ -140,7 +140,7 @@ function addTask(taskName, taskDueDate) {
   renderTasks();
 
   // フォームをリセット
-  let taskFormElm = document.querySelector("#taskForm");
+  let taskFormElm = document.querySelector('#taskForm');
   taskFormElm.reset();
 }
 
@@ -180,7 +180,7 @@ function toggleTaskComplete(taskName) {
 }
 
 function loadTasks() {
-  let jsonString = window.localStorage.getItem("tasks");
+  let jsonString = window.localStorage.getItem('tasks');
   if (jsonString) {
     tasks = JSON.parse(jsonString);
   }
@@ -188,37 +188,30 @@ function loadTasks() {
 
 function saveTasks() {
   let jsonString = JSON.stringify(tasks);
-  window.localStorage.setItem("tasks", jsonString);
+  window.localStorage.setItem('tasks', jsonString);
 }
 
 // CSV出力機能
 function csvExport() {
   // タスクが登録されていない場合、警告を出す
   if (tasks.length == 0) {
-    alert("出力するタスクがありません。");
+    alert('出力するタスクがありません。');
     return;
   }
-  let fileName = "export.csv";
-  let outputString = "タスク名,期日,進捗状況\n";
+  let fileName = 'export.csv';
+  let outputString = 'タスク名,期日,進捗状況\n';
   const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
 
   for (let task of tasks) {
-    let completedString = "";
+    let completedString = '';
     if (task.isCompleted) {
-      completedString = "済";
+      completedString = '済';
     } else {
-      completedString = "未";
+      completedString = '未';
     }
-    outputString =
-      outputString +
-      task.name +
-      "," +
-      task.dueDate +
-      "," +
-      completedString +
-      "\n";
+    outputString = outputString + task.name + ',' + task.dueDate + ',' + completedString + '\n';
   }
-  const blob = new Blob([bom, outputString], { type: "text/csv" });
+  const blob = new Blob([bom, outputString], { type: 'text/csv' });
   //IE10/11用(download属性が機能しないためmsSaveBlobを使用）
   if (window.navigator.msSaveBlob) {
     window.navigator.msSaveBlob(blob, fileName);
@@ -226,7 +219,7 @@ function csvExport() {
     //BlobからオブジェクトURLを作成する
     const url = (window.URL || window.webkitURL).createObjectURL(blob);
     //ダウンロード用にリンクを作成する
-    const download = document.createElement("a");
+    const download = document.createElement('a');
     //リンク先に上記で生成したURLを指定する
     download.href = url;
     //download属性にファイル名を指定する
@@ -245,17 +238,17 @@ function csvImport() {
     console.log(reader.result);
     let importTasks = reader.result.split(/\r\n|\r|\n/);
     for (let rawTask of importTasks) {
-      let taskArray = rawTask.split(",");
+      let taskArray = rawTask.split(',');
       if (taskArray.length != 3) {
         continue;
       }
-      let isCompleted = taskArray[2] == "済";
+      let isCompleted = taskArray[2] == '済';
       let task = {
         name: taskArray[0],
         dueDate: taskArray[1],
         isCompleted: isCompleted,
       };
-      if (task.name == "タスク名") {
+      if (task.name == 'タスク名') {
         continue;
       }
       let taskIndex = getIndexTasks(task.name);
